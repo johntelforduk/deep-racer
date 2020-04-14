8f1cc88ac6d4a8639cd4ab2b3814fed17bcaae82
johntelforduk <jtelford1@gmail.com>
2020-04-14 15:54:51 +0100

    Use unittest.

--- test_parse_logs.py
+++ test_parse_logs.py
@@ -1,24 +1,37 @@
 # Some tests of the log parsing code.
 
 import parse_logs as pl
-
-def test1():
-    print(pl.filename_to_list_of_strings('test_data_1.txt'))
+import unittest
 
 
-def test2():
-    logs = pl.filename_to_list_of_strings('simulation_log.txt')
-    print(pl.filter_by_2nd_item(logs, 'TRACE_WAYPOINTS'))
+class TestParseLogs(unittest.TestCase):
+
+    def test_filename_to_list_of_strings(self):
+        self.assertEqual(pl.filename_to_list_of_strings('example_data_1.txt'), [['This', 'is', 'a'], ['test.']])
+
+    def test_filter_by_2nd_item(self):
+        logs = pl.filename_to_list_of_strings('example_short_log.txt')
+        filtered_logs = pl.filter_by_2nd_item(logs, 'TRACE_WAYPOINTS')
+
+        for log_entry in filtered_logs:
+            self.assertEqual(log_entry[1], 'TRACE_WAYPOINTS')
+
+    def test_make_list_of_waypoints(self):
+        logs = pl.filename_to_list_of_strings('example_short_log.txt')
+        waypoints_only = pl.filter_by_2nd_item(logs, 'TRACE_WAYPOINTS')
+        waypoints = pl.make_list_of_waypoints(waypoints_only)
+
+        for each_dict in waypoints:
+            keys_list = list(each_dict.keys())
+            keys_list.sort()
+            self.assertEqual(keys_list, ['waypoint', 'x', 'y'])
+
+    def test_make_list_of_statuses(self):
+        logs = pl.filename_to_list_of_strings('example_short_log.txt')
+        statuses_only = pl.filter_by_2nd_item(logs, 'TRACE_STATUS')
+        los = pl.make_list_of_statuses(statuses_only)
+        self.assertEqual(len(los), 4)
 
 
-def test3():
-    logs = pl.filename_to_list_of_strings('simulation_log.txt')
-    waypoints_only = pl.filter_by_2nd_item(logs, 'TRACE_WAYPOINTS')
-    print(pl.make_list_of_waypoints(waypoints_only))
-
-
-def test4():
-    logs = pl.filename_to_list_of_strings('simulation_log.txt')
-    statuses_only = pl.filter_by_2nd_item(logs, 'TRACE_STATUS')
-    los = pl.make_list_of_statuses(statuses_only)
-    print(len(los))
+if __name__ == '__main__':
+    unittest.main()
